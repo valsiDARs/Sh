bf165e71077cd9666af9d84044b40ee0b23604b2
Shi <vladislav.shishatskiy@gmail.com>
2022-01-10 14:10:37 +0200

    add comment

--- src/com/shpp/p2p/cs/vshishatskiy/assigment2/Assigment2Part1.java
+++ src/com/shpp/p2p/cs/vshishatskiy/assigment2/Assigment2Part1.java
@@ -1,3 +1,9 @@
+/**
+ * File: Assigment2Part1.java
+ *
+ * Program find roots quadratic equation through the discriminant
+ */
+
 package com.shpp.p2p.cs.vshishatskiy.assigment2;
 
 import com.shpp.cs.a.console.TextProgram;
@@ -15,13 +21,13 @@ public class Assigment2Part1 extends TextProgram {
 
     @Override
     public void run() {
-        lisConsole();
+        listenTerminal();
         calculatingDiscriminant(A,B,C);
         calculatingRoots(DISCRIMINANT, A, B);
         printRoots(DISCRIMINANT);
     }
 
-    private void lisConsole(){
+    private void listenTerminal(){
         print("Please enter a: ");
         A = readDouble();
         print("Please enter b: ");
@@ -29,15 +35,29 @@ public class Assigment2Part1 extends TextProgram {
         print("Please enter c: ");
         C =readDouble();
     }
+
+    /**
+     * @param a,b,c - coefficient, A*(x^2) + B*x + C = 0
+     */
+
     private void calculatingDiscriminant(double a, double b, double c){
         DISCRIMINANT = Math.pow(b, 2) - 4 * a * c;
     }
 
     private void calculatingRoots(double discriminant,double a, double b){
+        if (DISCRIMINANT < 0){
+            return;
+        }
         double denominator = 2 * a;
         FIRST_ROOT = (-b + Math.sqrt(discriminant) ) / denominator;
         SECOND_ROOT = (-b - Math.sqrt(discriminant) ) / denominator;
     }
+
+    /**
+     * If the discriminant is greater than zero, then the equation has two roots.
+     * If the discriminant is zero, then the equation has one root.
+     * If the discriminant is less than zero, then the equation has no roots.
+     */
 
     private void printRoots(double discriminant){
         if(discriminant < 0){
